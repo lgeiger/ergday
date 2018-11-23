43c7fa056ab6b4398c81bedb0c551445de0d7869
Lukas Geiger <lukas.geiger94@gmail.com>
2018-11-23 21:35:27 +0100

    First prototype

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,29 @@
-export default () => <div>Hello World.</div>;
+import * as React from "react";
+
+function getRandomWorkout() {
+  const workouts = require("../data/programs.json");
+  return workouts[0];
+}
+
+export default class extends React.Component {
+  static async getInitialProps() {
+    const workout = getRandomWorkout();
+    return { workout };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Workout of the day</h1>
+        <div>
+          <h2>{this.props.workout.title}</h2>
+          <ul>
+            {this.props.workout.program.map(item => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
